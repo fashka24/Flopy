2ecd20c08d6aa18f8bf701bcec55231c9ff1374a
fashka24 <sakirovdavyd@gmail.com>
2025-05-03 23:54:22 +0300

    Update STL

--- src/plugins/STD.hpp
+++ src/plugins/STD.hpp
@@ -52,18 +52,13 @@ namespace flopy {
 
             return XValue(0);
         }},
-                {"str", [](std::vector<XValue>& args) {
+                {"str", [this](std::vector<XValue>& args) {
                     XValue temp{0};
 
                     if (args.size() <= 0 && args.size() >= 2)
                         throw std::runtime_error("too few arguments for function \"str\"");
 
-                    if (args[0].get_type() == XValueType::INT)
-                        temp = XValue(std::to_string(args[0].get<int>()), XValueType::STRING);
-                    else if (args[0].get_type() == XValueType::FLOAT)
-                        temp = XValue(std::to_string(args[0].get<float>()), XValueType::STRING);
-                    else
-                        temp = args[0];
+                    temp = XValue(this->xvalue2str(args[0]), XValueType::STRING);
 
                     return temp;
                 }},
